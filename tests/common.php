3dcf52992e8a7c632dc7aa9c87a4578e9851a61c
Francesco Bianco <info.francescobianco@gmail.com>
2015-10-12 13:06:07 +0200

    roadmap

--- tests/common.php
+++ tests/common.php
@@ -6,8 +6,13 @@ ini_set('display_errors',true);
 if (function_exists('xdebug_disable')) { xdebug_disable(); }
 
 ##
-$host = 'm-04.th.seeweb.it';	## database host 
-$user = 'javanile02517';		## database username
-$pass = 'java17481';			## database password
-$name = 'javanile02517';		## database name	
-$pref = 'd1_';					## table prefix
+if (!file_exists(__DIR__.'/override.php')) {
+
+	##
+	$host = '<<host>>';	## database host 
+	$user = '<<user>>';	## database username
+	$pass = '<<pass>>';	## database password
+	$name = '<<name>>';	## database name	
+	$pref = '<<pref>>'; ## database table prefix
+}
+
