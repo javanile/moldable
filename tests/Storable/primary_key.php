6f97246d9cb5835842e2d273fb88887ddee0bcf9
Francesco Bianco <info.francescobianco@gmail.com>
2015-10-13 11:50:09 +0200

    load by main field fixed

--- tests/Storable/primary_key.php
+++ tests/Storable/primary_key.php
@@ -6,19 +6,16 @@ require_once 'common.php';
 ##
 use Javanile\SchemaDB;
 
-##
-class Invoices extends SchemaDB\Storable {
+## extremely coincise model definition
+class Invoices extends SchemaDB\Storable 
+{	
+	## with a private key field
+	public $id = self::PRIMARY_KEY;
 	
-	##
-	public $id = self::PRIMARY_KEY; ## 
-	
-	##
+	## . . . 
 	public $number = 0;
-	
-	##
-	public $year = 0;
-	
-	##
+		
+	## . . .
 	public $created = self::DATE;
 }
 
