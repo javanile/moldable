ea970b9322394810769b1cab985d6e8d6867506f
francescobianco <info.francescobianco@gmail.com>
2017-04-03 23:58:20 +0200

    trash removed

--- trash/config.sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//
-$host     = '<<host>>';     // database host
-$dbname   = '<<dbname>>';   // database name
-$username = '<<username>>'; // database username
-$password = '<<password>>'; // database password
-$prefix   = '<<pref>>';     // database table prefix
-
