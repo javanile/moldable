8b0f111c88182636bd52781b11a083430ccdfe86
francescobianco <info.francescobianco@gmail.com>
2017-08-03 22:48:38 +0200

    version 0.0.1

---
+++ src/Database/RawApi.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Collect API to handle fields of a model.
+ *
+ * PHP version 5.6
+ *
+ * @author Francesco Bianco
+ */
+namespace Javanile\Moldable\Database;
+
+trait RawApi
+{
+    /**
+     * Retrieve primary key name of specific model.
+     *
+     * @param type $model
+     * @return type
+     */
+    public function raw($model)
+    {
+        // describe the model
+        $desc = $this->desc($model);
+
+        // search by fields for primary key
+        foreach ($desc[$model] as $field => $aspects) {
+            if ($aspects['Key'] == 'PRI') {
+                return $field;
+            }
+        }
+    }
+}
---
+++ src/Model/RawApi.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Trait with utility methods to handle errors.
+ *
+ * PHP version 5.6
+ *
+ * @author Francesco Bianco
+ */
+namespace Javanile\Moldable\Model;
+
+trait RawApi
+{
+    /**
+     *
+     *
+     *
+     *
+     * @param type $array
+     */
+    protected static function raw(
+        $sql,
+        $params = null,
+        $singleRecord = false,
+        $singleValue = false,
+        $casting = true
+    ) {
+        // requested a single record
+        if ($singleRecord && !$singleValue && $casting) {
+            $record = static::getDatabase()->getRow($sql, $params);
+            return $record ? static::make($record): null;
+        } else if (!$singleRecord && !$singleValue) {
+            // requested a multiple records many value per records
+            $records = static::getDatabase()->getResults($sql, $params);
+
+            if (!$records) {
+                return;
+            }
+
+            //
+            if ($casting) {
+                foreach($records as &$record) {
+                    $record = static::make($record);
+                }
+            }
+
+            return $records;
+
+        } else if ($singleRecord && $singleValue) {
+            // requested a single value of a single record
+            $value = static::getDatabase()->getValue($sql, $params);
+
+            return $value;
+        }
+    }
+}
