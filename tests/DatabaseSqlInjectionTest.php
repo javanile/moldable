002ccb4d7aef9956b19181f144a34f5d22395e8f
francescobianco <info.francescobianco@gmail.com>
2017-08-03 22:57:26 +0200

    version 0.0.1

---
+++ tests/DatabaseSqlInjectionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Javanile\Moldable\Tests;
+
+use Javanile\Producer;
+use Javanile\Moldable\Database;
+use PHPUnit\Framework\TestCase;
+
+Producer::addPsr4(['Javanile\\Moldable\\Tests\\' => __DIR__]);
+
+final class DatabaseSqlInjectionTest extends TestCase
+{
+    use DatabaseTrait;
+
+    public function testSqlInjectIntoInsertQuery()
+    {
+        $db = new Database([
+            'socket'   => 'pdo',
+            'host'     => $GLOBALS['DB_HOST'],
+            'dbname'   => $GLOBALS['DB_NAME'],
+            'username' => $GLOBALS['DB_USER'],
+            'password' => $GLOBALS['DB_PASS'],
+            'prefix'   => 'prefix_',
+        ]);
+
+        $db->apply([
+            'test' => [
+                'x' => $db::TEXT,
+            ],
+        ]);
+
+        // try a Sql Injection
+        $db->insert('test', [
+            "x) SELECT CONCAT('The MySQL version is: ', VERSION()) -- " => "dummy value",
+            "x" => "dummy value",
+        ]);
+
+        $this->assertTrue(is_object($results[0]));
+
+        $this->assertTrue(is_object($results[1]));
+    }
+}
