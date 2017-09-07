00d060f7b8ceb4bae46edbf9c3f65f75cc7d60ac
francescobianco <info.francescobianco@gmail.com>
2017-09-07 15:06:11 +0200

    Version 0.0.2

--- src/Exception.php
+++ src/Exception.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * 
- * 
+ * Class that handle a connection with database.
+ *
+ * PHP version 5.6
+ *
+ * @author Francesco Bianco
  */
-
 namespace Javanile\Moldable;
 
 class Exception extends \Exception
