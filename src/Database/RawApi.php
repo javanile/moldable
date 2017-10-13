93287f611e3d42a17b01a8285407e3296b4cb783
Francesco Bianco <info.francescobianco@gmail.com>
2017-10-13 23:15:41 +0200

    Update RawApi.php

--- src/Database/RawApi.php
+++ src/Database/RawApi.php
@@ -11,6 +11,15 @@ namespace Javanile\Moldable\Database;
 
 trait RawApi
 {
+    /*\
+    > Il modo in cui usi i trait è sbagliato. Un trait non dovrebbe mai 
+    > dipendere da altri trait o da funzionalità di una classe base. Dentro la 
+    > cartella Model, per esempio, nessuno dei trait può essere usato 
+    > indipendentemente dagli altri. Tanto vale fare una classe unica, o usare 
+    > composizione di classi, e non "finta ereditarietà multipla".
+    > (Anonimo) 
+    \*/
+    
     /**
      * Retrieve primary key name of specific model.
      *
